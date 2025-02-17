335eb109cd41a154e15b86de8e47a3e3859663ee
Mukul Joshi <145245946+HeyMeMukul@users.noreply.github.com>
2025-02-18 00:50:36 +0530

    Update settings.py

--- ctf_platform/ctf_platform/settings.py
+++ ctf_platform/ctf_platform/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&ua*xy3m9usm%t-b5rq%0r!7o7t04qdh!@nfeolvnr!*m&n&)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = True
+DEBUG = False
+# DEBUG = True
 ALLOWED_HOSTS = ['ctf-platform-fe9844f01013.herokuapp.com']
 # ALLOWED_HOSTS = ['*']
 
